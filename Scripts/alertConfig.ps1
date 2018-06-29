86fa7871c9dac3ef181a95374ae7485f9035da93
Kristian Nese <kristiannese@live.com>
2018-06-29 13:24:13 +0200

    update

--- Scripts/alertConfig.ps1
+++ Scripts/alertConfig.ps1
@@ -1,25 +1,48 @@
-Write-Output "Selecting customer subscription..."
-
-Write-output "who am i?"
-get-azurermcontext
-
-Get-AzureRmSubscription -TenantId b2a0bb8e-3f26-47f8-9040-209289b412a8
-
-Select-AzureRmSubscription -SubscriptionId 4b1a121f-fd0e-4a21-94ef-1d246437a7ca -TenantId b2a0bb8e-3f26-47f8-9040-209289b412a8
-
-Write-Output "Updating VM alert config per new commit..."
-
-$VMs = Get-AzureRmVm
-
-Foreach ($vm in $vms)
+Try
 {
-    Write-Output "Deploying default alerts...."
 
-    New-AzureRmResourceGroupDeployment -Name "vmAlert" `
-                                       -ResourceGroupName $vm.ResourceGroupName `
-                                       -TemplateUri 'https://raw.githubusercontent.com/krnese/managedServices/master/Templates/azureClassicAlert.json' `
-                                       -resourceId $vm.id `
-                                       -Verbose
+    Write-Output "Selecting customer subscription..."
 
-    Write-Output "Done!"                                       
+    Get-AzureRmSubscription -TenantId b2a0bb8e-3f26-47f8-9040-209289b412a8
+
+    Select-AzureRmSubscription -SubscriptionId 4b1a121f-fd0e-4a21-94ef-1d246437a7ca -TenantId b2a0bb8e-3f26-47f8-9040-209289b412a8
 }
+Catch
+{
+    $ErrorMessage = 'Failed to logon to Azure...'
+    $ErrorMessage += "`n"
+    $ErrorMessage += $_
+    Write-Error -Message $ErrorMessage `
+                -ErrorAction Stop
+}
+
+    Write-Output "Updating VM alert config per new commit..."
+
+    $VMs = Get-AzureRmVm
+
+Try
+{  
+    Foreach ($vm in $vms)
+    {
+        Write-Output "Deploying default alerts...."
+
+        New-AzureRmResourceGroupDeployment -Name "vmAlert" `
+                                        -ResourceGroupName $vm.ResourceGroupName `
+                                        -TemplateUri 'https://raw.githubusercontent.com/krnese/managedServices/master/Templates/azureClassicAlert.json' `
+                                        -resourceId $vm.id `
+                                        -Verbose
+
+        Write-Output "Done!"                                       
+    }
+}
+Catch
+{
+    $ErrorMessage = 'Unable to update alert on VM:'
+    $ErrorMessage += "`n"
+    $ErrorMessage += $vm.Name
+    $ErrorMessage += $_
+    Write-Error -Message $ErrorMessage `
+                -ErrorAction Stop
+}
+
+Write-Output "Job completed"
